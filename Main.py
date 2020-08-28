a3e638eef6063cf8ede78db6de496fba2f3ce0ec
Telavus <joakim@idland.io>
2020-08-28 21:36:21 +0200

    screen pang pang!

--- Main.py
+++ Main.py
@@ -1,15 +1,26 @@
 import sys, pygame
+from pygame.locals import *
+
+
 pygame.init()
 
 clock = pygame.time.Clock()
 size = width, height = 1280, 720
 screen = pygame.display.set_mode(size)
+screencolor = (0,0,0)
 
 while 1:
     clock.tick(60)
+    screen.fill(screencolor)
 
+    pressed = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    screen.fill((100, 0, 0))
+    if pressed[K_SPACE]:
+        screencolor = (100,0,0)
+    else:
+            screencolor = (0,0,0)
+
+
     pygame.display.flip()
